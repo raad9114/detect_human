7eeca5716095c0636b73a4db5101bb676e17fd37
Zaoyad Khan Raad <raad9114@gmail.com>
2022-08-01 15:06:19 +0600

    first commit

---
+++ human_detect.py
@@ -0,0 +1,22 @@
+import mediapipe as mediapipe  #pip install mediapipe
+import cv2 #pip install opencv-python
+
+#declare variables
+readVideo = cv2.VideoCapture(0) #video capture variable
+mediapipePose = mediapipe.solutions.pose #mediapipe pose variable
+poseDetect = mediapipePose.Pose() #mediapipe pose variable
+mediapipeDrawingUtils = mediapipe.solutions.drawing_utils #mediapipe draw variable
+
+#make the conditions 
+while True:
+    show,img = readVideo.read()
+    if not show:
+        break
+    imgRGB = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    results = poseDetect.process(imgRGB)
+
+    mediapipeDrawingUtils.draw_landmarks(img,results.pose_landmarks,mediapipePose.POSE_CONNECTIONS)
+
+    cv2.imshow('Image',img)
+    if cv2.waitKey(1) & 0xFF==ord('q'):
+        break
